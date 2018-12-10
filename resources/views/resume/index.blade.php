5c26fc3d98e0e173aa7349ea25284cf445a7d119
rmnccit <rizamn@ymail.com>
2018-12-10 10:19:57 +0700

    Initial commit

---
+++ resources/views/resume/index.blade.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Resume - Riza Muhammad Nurman</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('resume/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+    <link href="{{asset('resume/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset('resume/css/resume.min.css')}}" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Riza Muhammad Nurman</span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="{{asset('resume/img/profile.jpg')}}" alt="">
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#interests">Organization</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#awards">Awards</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid p-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+          <h1 class="mb-0">Riza
+            <span class="text-primary">Muhammad Nurman</span>
+          </h1>
+          <div class="subheading mb-5">Jl. Raya Loji Gg. Sawo No 263 · Bogor 16617 · 081280039829 ·
+            <a href="mailto:rizaman007@gmail.com">rizaman007@gmail.com</a>
+          </div>
+          <p class="lead mb-5">I am experienced in lecturing especially in IT field. I also like to create programs using many programming languages.</p>
+          <div class="social-icons">
+            <a href="https://www.slideshare.net/rizaman" target="_blank">
+              <i class="fab fa-linkedin-in"></i>
+            </a>
+            <a href="https://twitter.com/rhyzone" target="_blank">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="https://web.facebook.com/rizaman?_rdc=1&_rdr" target="_blank">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <hr class="m-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+        <div class="my-auto">
+          <h2 class="mb-5">Experience</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Vice of Curricullum Coordinator</h3>
+              <div class="subheading mb-3">CEP CCIT FTUI</div>
+              <p>Helping create a new curriculum of IT, such as subject, syllabus, modules, content, etc.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">February 2018 - Present</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Faculty Staff</h3>
+              <div class="subheading mb-3">CEP CCIT FTUI</div>
+              <p>Coordinate with the Academic Administration. I'm responsible for teaching and learning process and assessment of learning outcomes.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">February 2008 - Present</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Technical Staff</h3>
+              <div class="subheading mb-3">PT Miskat Alam Konsultan</div>
+              <p>Create expert staff documents to be included in the project proposal.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">June 2007 - August 2007</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Graphic Designer</h3>
+              <div class="subheading mb-3">CV Rajawali Corporation</div>
+              <p>Responsible for designing document layouts.</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">January 2006 - December 2006</span>
+            </div>
+          </div>
+
+        </div>
+
+      </section>
+
+      <hr class="m-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+        <div class="my-auto">
+          <h2 class="mb-5">Education</h2>
+
+          <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Institut Pertanian Bogor</h3>
+              <div class="subheading mb-3">Bachelor of Computer Science</div>
+              <div>Computer Science Department FMIPA</div>
+              
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">June 2002 - February 2007</span>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <hr class="m-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+        <div class="my-auto">
+          <h2 class="mb-5">Skills</h2>
+          <div class="subheading mb-3">Programming</div>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Java, Servlet, JSP, JSF, Hibernate</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              VB, C#, ASP.NET</li>
+            <li>
+                <i class="fa-li fa fa-check"></i>
+                PHP, Laravel</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              SQL Server, MySQL</li>
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              CSS, JavaScript</li>
+          </ul>
+          <br/>
+          <div class="subheading mb-3">Design</div>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-check"></i>
+              Photoshop, CorelDraw</li>
+            </ul>
+        <br/>
+          <div class="subheading mb-3">Workflow</div>
+          <ul class="fa-ul mb-0">
+              <li>
+                <i class="fa-li fa fa-check"></i>
+                Software Development Life Cycle</li>
+                <li>
+                    <i class="fa-li fa fa-check"></i>
+                    Unified Modelling Language</li>
+                <li>
+                    <i class="fa-li fa fa-check"></i>
+                    Software Quality Assurance</li>
+              </ul>
+        </div>
+      </section>
+
+      <hr class="m-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+        <div class="my-auto">
+          <h2 class="mb-5">Organizations</h2>
+          <p>Founder with others to build Unit Kegiatan Mahasiswa Catur IPB - CUA (Chess Unity of Agriculture) 2006</p>
+          <p>Secretary of CUA 2006</p>
+          <p>Committe of Graduation Ceremony CEP CCIT FTUI</p>
+          <p>Head of Committee of Seminar Series CEP CCIT FTUI 2017</p>
+        </div>
+      </section>
+
+      <hr class="m-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="awards">
+        <div class="my-auto">
+          <h2 class="mb-5">Awards &amp; Certifications</h2>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              2005, El Rahma Education Centre - Grapic Design Course as Student</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              2008, CEP CCIT FTUI - Building LAN for Office/Warnet as Instructor</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              2008, Kementrian Pertahanan RI - Diklat Teknisi Komputer as Instructor</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              2009, Kementrian Pertahanan RI - Diklat Web Design as Instructor </li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              2009, Kementrian Pertahanan RI - Diklat Programmer Komputer as Instructor</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              2010, NIIT and SUN microsystem - Sun Educational Service Course as Student</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              2012, CEP CCIT FTUI, UIN, Tazkia - Sharia Banking Information Technology as Student</li>
+              <li>
+                <i class="fa-li fa fa-trophy text-warning"></i>
+                2013, NIIT - Smartphone Application development as Student</li>
+            <li>
+                <i class="fa-li fa fa-trophy text-warning"></i>
+                2014, Kementrian Pertahanan RI - Diklat Web Programming as Instructor</li>
+            <li>
+                <i class="fa-li fa fa-trophy text-warning"></i>
+                2015, UP2M Department Teknik Elektro FTUI - CompTIA Securiy+ as Student</li>
+            <li>
+                <i class="fa-li fa fa-trophy text-warning"></i>
+                2016, Develop CCIS (CCIT Information System) for Academic Activity</li>
+            <li>
+                <i class="fa-li fa fa-trophy text-warning"></i>
+                2017, Develop Official Website of CEP CCIT FTUI</li>
+            <li>
+                <i class="fa-li fa fa-trophy text-warning"></i>
+                2018, CEP CCIT FTUI Collaborate with NIIT - Java Programming as Instructor</li>
+            <li>
+                <i class="fa-li fa fa-trophy text-warning"></i>
+                2018, CEP CCIT FTUI - Faculty Terpuji</li>
+          </ul>
+        </div>
+      </section>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{asset('resume/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('resume/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="{{asset('resume/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="{{asset('resume/js/resume.min.js')}}"></script>
+
+  </body>
+
+</html>
